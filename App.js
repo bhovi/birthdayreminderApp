84de5563efd1ad350e335264f3f920d504b471b1
bhovi <umamaheshwari9019@gmail.com>
2023-01-13 15:49:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {data} from "./Birthday"
+function App() 
+{
+  const [people, setPeople] = useState(data)
+  const remove=(id)=>
+  {
+    let newPerson=people.filter((person) =>person.id!==id)
+    setPeople(newPerson)
+  }
+  return (
+    <>
+  <h3 style={{ backgroundColor: "yellow", color: "purple", textAlign: "center", fontSize:"40px" }}>
+      YOU HAVE {people.length} BIRTHDAY's TODAY</h3>
+    {
+    people.map((person) => {const { id, name, age, city, image } = person
+      return (
+        <div className="container">
+          <img src={image} alt={name} />
+          <ul>
+            <li>{name}</li>
+            <li>{age}</li>
+            <li>{city}</li>
+          </ul>
+          <button onClick={() => remove(id)}>DISMISS</button>
+          </div>
+    )
+  })}
+  </>
+  )
+}
+export default App
---
+++ Birthday.js
@@ -0,0 +1,72 @@
+export const data = [
+    {
+        id: 1,
+        name: "uma",
+        age: 22,
+        city: "mumbai",
+        image: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 2,
+        name: "suma",
+        age: 34,
+        city: "chennai",
+        image: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 3,
+        name: "ruma",
+        age: 12,
+        city: "bangalore",
+        image: "https://images.pexels.com/photos/3754438/pexels-photo-3754438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 4,
+        name: "huma",
+        age: 29,
+        city: "hyderbad",
+        image: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 5,
+        name: "buma",
+        age: 54,
+        city: "panjim",
+        image:"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 6,
+        name: "riya",
+        age: 31,
+        city: "mangalore",
+        image:"https://images.pexels.com/photos/3754438/pexels-photo-3754438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 7,
+        name: "tiya",
+        age: 30,
+        city: "baga",
+        image:"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 8,
+        name: "niya",
+        age: 29,
+        city: "colva",
+        image:"https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 9,
+        name: "radha",
+        age: 43,
+        city: "kolkata",
+        image:"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        id: 10,
+        name: "seetha",
+        age: 33,
+        city: "patna",
+        image:"https://images.pexels.com/photos/3754438/pexels-photo-3754438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }
+]
